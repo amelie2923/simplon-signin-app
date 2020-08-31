807c7868136e4f6ea8870655b098318aeb286b89
windtagane <windtagane@gmail.com>
2020-08-31 11:28:23 +0400

    async controller.dataSheets

--- config/database.js
+++ config/database.js
@@ -1,10 +1,10 @@
 let dbUrl = ''
-console.log("process.env")
-console.log(process.env.DB_URL)
 if (process.env.DB_URL) {
-     dbUrl = process.env.DB_URL
+    dbUrl = process.env.DB_URL
+    console.log(`Connecting to ${dbUrl}`);
 } else {
-  dbUrl='mongodb://localhost:27017/simplonPdfGen';
+    dbUrl='mongodb://localhost:27017/simplonPdfGen';
+    console.log(`Connecting to ${dbUrl}`);
 }
 
 module.exports = {
