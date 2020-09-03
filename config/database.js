3a99338300098bbf34f0e8271364762352b6a5b8
Amelie <amelie.salaun@gmail.com>
2020-09-03 15:25:34 +0400

    Remove db infos

--- config/database.js
+++ config/database.js
@@ -2,9 +2,6 @@ let dbUrl = ''
 if (process.env.DB_URL) {
     dbUrl = process.env.DB_URL
     console.log(`Connecting to ${dbUrl}`);
-} else {
-    dbUrl='mongodb+srv://amelie:test1234@cluster0.kuvj1.azure.mongodb.net/simplonPdfGen?retryWrites=true&w=majority';
-    console.log(`Connecting to ${dbUrl}`);
 }
 
 module.exports = {
