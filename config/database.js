67138a77e26dce0d747c847d978cf0884d568e19
baretabel <baretabel4@gmail.com>
2020-09-03 13:51:04 +0400

    Signatyre+Url

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ if (process.env.DB_URL) {
     dbUrl = process.env.DB_URL
     console.log(`Connecting to ${dbUrl}`);
 } else {
-    dbUrl='mongodb://localhost:27017/simplonPdfGen';
+    dbUrl='mongodb+srv://amelie:test1234@cluster0.kuvj1.azure.mongodb.net/simplonPdfGen?retryWrites=true&w=majority';
     console.log(`Connecting to ${dbUrl}`);
 }
 
